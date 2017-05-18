b700197d3c0e11320e42ddfd6da3b380743da0bd
mskmlevy <mskmlevy@icloud.com>
2017-05-18 13:41:57 -0500

    add files

---
+++ gradeapp.js
@@ -0,0 +1,23 @@
+let grade= document.getElementById('grade').value;
+
+
+function calculateGrade(grade){
+  if (grade<60&&grade>=0){
+    console.log('F');
+  }
+    if (grade>=60&&grade<70){
+      console.log('D');
+    }
+      if (grade>=70&&grade<80){
+        console.log('C');
+      }
+      if (grade>=80&&grade<90){
+        console.log('B');
+      }
+      if (grade>=90&&grade<=100){
+        console.log('A');
+      }
+      else {
+        console.log('Invalid Entry');
+      }
+}
