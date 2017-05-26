8c11091a81e41e47ce0ec10e01e338979c21bbc6
mskmlevy <mskmlevy@icloud.com>
2017-05-26 05:53:51 -0500

    Update and clean up
    cleaned up code

--- gradeapp.js
+++ gradeapp.js
@@ -1,23 +1,20 @@
-let grade= document.getElementById('grade').value;
+function calculateGrade() {
+  let grade = document.getElementById('grade1').value;
 
-
-function calculateGrade(grade){
-  if (grade<60&&grade>=0){
+  if (grade < 60 && grade >= 0) {
     console.log('F');
+  } else if (grade >= 60 && grade < 70) {
+    console.log('D');
+  } else if (grade >= 70 && grade < 80) {
+    console.log('C');
+  } else if (grade >= 80 && grade < 90) {
+    console.log('B');
+  } else if (grade >= 90 && grade <= 100) {
+    console.log('A');
+  } else {
+    console.log('Invalid Entry');
   }
-    if (grade>=60&&grade<70){
-      console.log('D');
-    }
-      if (grade>=70&&grade<80){
-        console.log('C');
-      }
-      if (grade>=80&&grade<90){
-        console.log('B');
-      }
-      if (grade>=90&&grade<=100){
-        console.log('A');
-      }
-      else {
-        console.log('Invalid Entry');
-      }
+
+  // Write grade to page
+  // limit input values to 0-100 reject all other values
 }
