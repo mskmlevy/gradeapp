fd0f1fcd2e5f340659eb5e5c6bdb910e3ea849e2
mskmlevy <mskmlevy@icloud.com>
2017-06-14 20:45:16 -0500

    Print to DOM
    This update gets the result to print to DOM. I've been toying with it
    forever to get it to go from console log to actually print in the
    window.

--- gradeapp.js
+++ gradeapp.js
@@ -1,18 +1,19 @@
+
 function calculateGrade() {
   let grade = document.getElementById('grade1').value;
 
   if (grade < 60 && grade >= 0) {
-    console.log('F');
+        document.getElementById('showGrade').innerHTML = 'F';
   } else if (grade >= 60 && grade < 70) {
-    console.log('D');
+        document.getElementById('showGrade').innerHTML = 'D';
   } else if (grade >= 70 && grade < 80) {
-    console.log('C');
+    document.getElementById('showGrade').innerHTML = 'C';
   } else if (grade >= 80 && grade < 90) {
-    console.log('B');
+    document.getElementById('showGrade').innerHTML = 'B';
   } else if (grade >= 90 && grade <= 100) {
-    console.log('A');
+    document.getElementById('showGrade').innerHTML = 'A';
   } else {
-    console.log('Invalid Entry');
+    document.getElementById('showGrade').innerHTML = 'Invalid Entry';
   }
 
   // Write grade to page
